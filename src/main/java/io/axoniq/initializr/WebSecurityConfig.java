c800143797253e49513fa815d4ac54872b0fa7e6
brunchey <ben.runchey@axoniq.io>
2021-05-27 15:45:44 -0500

    Attempting to add security to app.

---
+++ src/main/java/io/axoniq/initializr/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package io.axoniq.initializr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                //.antMatchers("/actuator/health").permitAll()
+                .antMatchers("/actuator/**").hasRole("METRICSADMIN")
+                .antMatchers("/**").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+    }
+}
+
