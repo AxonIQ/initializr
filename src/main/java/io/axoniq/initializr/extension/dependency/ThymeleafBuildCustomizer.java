7e1832323bf01d82778c0402de66f986907732f6
Ivan Dugalic <ivan.dugalic@axoniq.io>
2021-05-28 11:23:33 +0200

    #8 Added Thymeleaf customizer.
    Java Doc added.

--- src/main/java/io/axoniq/initializr/extension/dependency/ThymeleafBuildCustomizer.java
+++ src/main/java/io/axoniq/initializr/extension/dependency/ThymeleafBuildCustomizer.java
@@ -4,6 +4,11 @@ import io.spring.initializr.generator.buildsystem.Build;
 import io.spring.initializr.generator.buildsystem.Dependency;
 import io.spring.initializr.generator.spring.build.BuildCustomizer;
 
+/**
+ * A build customizer for Axon projects that provides explicit selection of the `thymeleaf-extras-spring-security` if `thymeleaf` and `security` is selected.
+ *
+ * @author Иван Дугалић
+ */
 public class ThymeleafBuildCustomizer implements BuildCustomizer<Build> {
 
     @Override
