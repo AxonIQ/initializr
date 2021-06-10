a2c48639e55b0969ed7aea6b284c882f157af8f4
Ivan Dugalic <ivan.dugalic@axoniq.io>
2021-06-10 08:55:59 +0200

    CORS configured - allow all methods

--- src/main/java/io/axoniq/initializr/InitializrApplication.java
+++ src/main/java/io/axoniq/initializr/InitializrApplication.java
@@ -23,7 +23,7 @@ public class InitializrApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }
