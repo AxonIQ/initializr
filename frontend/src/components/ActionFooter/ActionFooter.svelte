b0a2c4f6379d3f96804fb491067682ea1fb9fa3c
Dusan Perkovic <dusan.perkovic@axoniq.io>
2021-06-15 13:15:14 +0200

    Add download POC for ActionFooter.

--- frontend/src/components/ActionFooter/ActionFooter.svelte
+++ frontend/src/components/ActionFooter/ActionFooter.svelte
@@ -4,9 +4,52 @@ import Button from "../Button/Button.svelte";
 
 let shareVisible = false;
 
+const defaultHeaders = new Headers();
+defaultHeaders.append("Content-Type", "application/zip");
+defaultHeaders.append("Content-Disposition", "attachment");
+
+async function download() {
+    const response = await fetch(
+        'http://localhost:8080/starter.zip',
+        {
+            method: 'POST',
+            headers: defaultHeaders,
+            body: JSON.stringify({
+                applicationName: '',
+                artifactId: '',
+                // ???
+                // baseDir: '',
+                bootVersion: '',
+                dependencies: [],
+                description: '',
+                groupId: '',
+                javaVersion: '',
+                language: '',
+                name: '',
+                packageName: '',
+                packaging: '',
+                type: '',
+                version: ''
+            }),
+        }
+    );
+    response.headers.forEach(console.log);
+    // console.log(response.headers.get('content-disposition'));
+    const blob = await response.blob();
+    const file = window.URL.createObjectURL(blob);
+    
+    const fileLink = document.createElement('a');
+    fileLink.href = file;
+    fileLink.download = 'axon';
+    fileLink.click();
+}
+
 </script>
     <div class="action-footer">
-        <Button text="Download" />
+        <Button
+            text="Download"
+            onClick={() => download()}
+        />
         <Button
             text="Share"
             onClick={() => shareVisible = true}
