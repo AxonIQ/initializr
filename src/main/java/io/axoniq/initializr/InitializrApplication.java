26f5616e1052f5b1096f8c89c040f01a9e56882d
Lucas Campos <lfgcampos@gmail.com>
2021-06-22 15:55:00 +0200

    Allowed CORS on BE

--- src/main/java/io/axoniq/initializr/InitializrApplication.java
+++ src/main/java/io/axoniq/initializr/InitializrApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAutoConfiguration
 @SpringBootConfiguration
@@ -23,4 +25,13 @@ public class InitializrApplication {
         return new ProjectGenerationMonitor(new AxonProjectRequestDocumentFactory(), meterRegistry);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
+    }
 }
