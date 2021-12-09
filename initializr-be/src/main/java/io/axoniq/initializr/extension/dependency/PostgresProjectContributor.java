eb536424a72dd56d21558cb94776789ff6b2386c
Stefan Dragisic <stefan.dragisic@axoniq.io>
2021-12-09 12:40:53 +0100

    postgresql

---
+++ initializr-be/src/main/java/io/axoniq/initializr/extension/dependency/PostgresProjectContributor.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2021. AxonIQ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.axoniq.initializr.extension.dependency;
+
+import io.spring.initializr.generator.project.contributor.ProjectContributor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+/**
+ * A {@link ProjectContributor} that creates a docker-compose file and add a default postgres properties when it is
+ * selected.
+ *
+ * @author Lucas Campos
+ * @author Stefan Dragisic
+ */
+public class PostgresProjectContributor implements ProjectContributor {
+
+    private static final Log logger = LogFactory.getLog(PostgresProjectContributor.class);
+
+    private Path projectRoot;
+
+    @Override
+    public void contribute(Path projectRoot) throws IOException {
+        this.projectRoot = projectRoot;
+        copyFile("configuration/axon-framework/postgres/docker-compose-postgres.yml",
+                 "src/main/docker/docker-compose-postgres.yml");
+        appendToFile("configuration/axon-framework/postgres/application.properties",
+                     "src/main/resources/application.properties");
+    }
+
+    private void appendToFile(String sourcePath, String destinationPath) throws IOException {
+        Resource resource = new ClassPathResource(sourcePath);
+        Path output = projectRoot.resolve(destinationPath);
+        FileWriter writer = new FileWriter(output.toFile(), true);
+        writer.append(System.getProperty("line.separator"));
+        try (Stream<String> stream = Files.lines(resource.getFile().toPath())) {
+
+            stream.forEach(line -> {
+                try {
+                    writer.append(line);
+                    writer.append(System.getProperty("line.separator"));
+                } catch (IOException e) {
+                    logger.error(e);
+                }
+            });
+        } catch (IOException e) {
+            throw e;
+        }
+        writer.flush();
+    }
+
+    private void copyFile(String source, String destination) throws IOException {
+        Resource resource = new ClassPathResource(source);
+        Path destinationPath = projectRoot.resolve(destination);
+
+        if (!Files.exists(destinationPath)) {
+            Files.createDirectories(destinationPath.getParent());
+        }
+
+        Files.copy(resource.getFile().toPath(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
+    }
+}
