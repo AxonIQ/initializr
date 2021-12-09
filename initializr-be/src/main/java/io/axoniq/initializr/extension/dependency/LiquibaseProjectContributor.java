1c920e8d23fda598d44ee0c3f639cbddd2c5128b
Stefan Dragisic <stefan.dragisic@axoniq.io>
2021-12-09 12:09:30 +0100

    Changes by lfgcampos

--- initializr-be/src/main/java/io/axoniq/initializr/extension/dependency/LiquibaseProjectContributor.java
+++ initializr-be/src/main/java/io/axoniq/initializr/extension/dependency/LiquibaseProjectContributor.java
@@ -17,10 +17,13 @@
 package io.axoniq.initializr.extension.dependency;
 
 import io.spring.initializr.generator.project.contributor.ProjectContributor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 /**
  * A {@link ProjectContributor} that creates the "db/changelog" resources directory when Liquibase is selected.
@@ -31,9 +34,27 @@ import java.nio.file.Path;
  */
 public class LiquibaseProjectContributor implements ProjectContributor {
 
+    private Path projectRoot;
+
     @Override
     public void contribute(Path projectRoot) throws IOException {
-        Path changelogDirectory = projectRoot.resolve("src/main/resources/db/changelog");
+        this.projectRoot = projectRoot;
+        Path changelogDirectory = this.projectRoot.resolve("src/main/resources/db/changelog");
         Files.createDirectories(changelogDirectory);
+
+        copyFile("configuration/axon-framework/liquibase/db.changelog-00001-0-create-axon-framework-related-tables-baseline.xml",
+                 "src/main/resources/db/migration/db.changelog-00001-0-create-axon-framework-related-tables-baseline.xml");
+
+    }
+
+    private void copyFile(String source, String destination) throws IOException {
+        Resource resource = new ClassPathResource(source);
+        Path destinationPath = projectRoot.resolve(destination);
+
+        if (!Files.exists(destinationPath)) {
+            Files.createDirectories(destinationPath.getParent());
+        }
+
+        Files.copy(resource.getFile().toPath(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
     }
 }
